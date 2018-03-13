ce6a7bac3c4c651be0a3af7c4828c67ee7b8e71b
AlexBardDev <alexandre.bardiaux@outlook.com>
2018-03-13 13:58:58 +0100

    PEP 8

--- anagram.py
+++ anagram.py
@@ -3,39 +3,39 @@ This script takes 2 parameters(=words) and tells if they are anagrams or not.
 """
 
 def anagram(chaine1, chaine2):
-	"""Function that checks if 2 words are anagrams or not"""
+    """Function that checks if 2 words are anagrams or not"""
 
-	list1 = list(chaine1)
-	list2 = list(chaine2)
+    list1 = list(chaine1)
+    list2 = list(chaine2)
 
-	i = 0
-	is_anagram = False
-	
-	while len(list1) == len(list2):
-		
-		if list1[i] in list2:
-			i += 1
-		else : 
-			break
+    i = 0
+    is_anagram = False
 
-		if i == len(list1)-1:
-			is_anagram = True
-			break
+    while len(list1) == len(list2):
 
-	return is_anagram
+        if list1[i] in list2:
+            i += 1
+        else:
+            break
+
+        if i == len(list1)-1:
+            is_anagram = True
+            break
+
+    return is_anagram
 
 while True:
-	word1 = input("First word : ")
-	word2 = input("Second word : ")
+    word1 = input("First word : ")
+    word2 = input("Second word : ")
 
-	if anagram(word1.lower(), word2.lower()) == True:
-		message = """{} and {} are anagrams""".format(word1, word2)
-	else:
-		message = """{} and {} are not anagrams""".format(word1, word2)
+    if anagram(word1.lower(), word2.lower()) == True:
+        message = """{} and {} are anagrams""".format(word1, word2)
+    else:
+        message = """{} and {} are not anagrams""".format(word1, word2)
 
-	print(message)
+    print(message)
 
-	choice = input("Do you want to quit ? (y/n) ")
+    choice = input("Do you want to quit ? (y/n) ")
 
-	if choice == "y":
-		break
+    if choice == "y":
+        break
